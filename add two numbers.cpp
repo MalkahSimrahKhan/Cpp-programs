43c88a19d8e738d1b3e12d3ec6521f0f99a86de0
MalkahSimrahKhan <120410760+MalkahSimrahKhan@users.noreply.github.com>
2022-12-13 05:51:36 +0000

    Add files via upload

---
+++ add two numbers.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+/*{//Add two numbers
+    int x,y,sum;
+    cout<< "Enter 1st number" << endl;
+    cin>>x;
+    cout<< "Enter 2nd number"<<endl;
+    cin>>y;
+    sum=x+y;
+    cout<<"sum="<<sum<<endl;
+    return 0;
+}*/
+
+/*{
+    //Get quotient of division of two numbers
+
+    int x,y,res;
+    cout<<"Enter first number" <<endl;
+    cin>>x;
+    cout<< "Enter second number"<<endl;
+    cin>>y;
+    res=x%y;
+    cout<<"Modulus="<<res<<endl;
+    return 0;
+}*/
+
+
+/*{
+    //Input 4  numbers and get sum of these
+
+    int n1,n2,n3,n4,sum;
+    cout<<"Enter 1st number"<<endl;
+    cin>>n1;
+    cout<<"Enter 2nd number"<<endl;
+    cin>>n2;
+    cout<<"Enter 3rd number"<<endl;
+    cin>>n3;
+    cout<<"Enter 4th number"<<endl;
+    cin>>n4;
+    sum=n1+n2+n3+n4;
+    cout<<"Sum="<<sum<<endl;
+    return 0;
+}*/
+
+/*{
+    //sum and average marks of five students
+
+    int m1,m2,m3,m4,m5,sum,avg;
+    cout<<"Enter marks of 1st student"<<endl;
+    cin>>m1;
+    cout<<"Enter marks of 2nd student"<<endl;
+    cin>>m2;
+    cout<<"Enter marks of 3rd student"<<endl;
+    cin>>m3;
+    cout<<"Enter marks of 4th student"<<endl;
+    cin>>m4;
+    cout<<"Enter marks of 5th student"<<endl;
+    cin>>m5;
+    sum=m1+m2+m3+m4+m5;
+    cout<<"Sum="<<sum<<endl;
+    avg=sum/5;
+    cout<<"Average marks of five students="<<avg<<endl;
+    return 0;
+}*/
+
+
+{
+    //
+}
