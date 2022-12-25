ca9dcb78e7423d0e7f22b88f1fdabcbbfbbcaa7c
MalkahSimrahKhan <120410760+MalkahSimrahKhan@users.noreply.github.com>
2022-12-25 11:11:43 +0000

    quotient of division of two numbers

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // task3 Get quotient of division of two numbers
+
+    int x,y,res;
+    cout<<"Enter first number" <<endl;
+    cin>>x;
+    cout<< "Enter second number"<<endl;
+    cin>>y;
+    res=x%y;
+    cout<<"Modulus="<<res<<endl;
+    return 0;
+}
