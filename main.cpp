57e1f8761cba9d667b68fd8e666ce1e9944d1d5d
MalkahSimrahKhan <120410760+MalkahSimrahKhan@users.noreply.github.com>
2022-12-25 11:19:12 +0000

    input four number from user and sum of them

--- main.cpp
+++ main.cpp
@@ -4,14 +4,16 @@ using namespace std;
 
 int main()
 {
-    // task3 Get quotient of division of two numbers
-
-    int x,y,res;
-    cout<<"Enter first number" <<endl;
-    cin>>x;
-    cout<< "Enter second number"<<endl;
-    cin>>y;
-    res=x%y;
-    cout<<"Modulus="<<res<<endl;
+    int n1,n2,n3,n4,sum;
+    cout<<"Enter 1st number"<<endl;
+    cin>>n1;
+    cout<<"Enter 2nd number"<<endl;
+    cin>>n2;
+    cout<<"Enter 3rd number"<<endl;
+    cin>>n3;
+    cout<<"Enter 4th number"<<endl;
+    cin>>n4;
+    sum=n1+n2+n3+n4;
+    cout<<"Sum="<<sum<<endl;
     return 0;
 }
